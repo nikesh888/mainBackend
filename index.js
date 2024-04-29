d98e5dbe6fc6dc3774be6219f83993d36dee1a69
nikesh888 <nikesh.nckinfotech@gmail.com>
2024-04-29 13:40:52 +0530

    restored

---
+++ index.js
@@ -0,0 +1,21 @@
+import bodyParser from "body-parser";
+import cors from "cors";
+app.use(cors({ origin: "*" }));
+import express from "express";
+import "./dbConnect.js";
+import adminRouter from "./routers/adminRouter.js";
+import scraperWs from "./websocket-scraper.js";
+
+const app = express();
+app.use(bodyParser.json({ limit: "50mb" }));
+const PORT = process.env.PORT || 5000;
+app.use(express.json()); //json body parser
+
+app.listen(PORT, () => console.log(`server started at port: ${PORT}`));
+
+app.get("/test", (req, res) => {
+  res.send("it works mate!");
+});
+scraperWs();
+
+app.use("/api", adminRouter);
