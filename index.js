546517061a9d51bf41fe5f40c17d5acd12882649
nikesh888 <nikesh.nckinfotech@gmail.com>
2024-04-29 13:44:19 +0530

    cors fix

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import bodyParser from "body-parser";
 import cors from "cors";
-app.use(cors({ origin: "*" }));
 import express from "express";
 import "./dbConnect.js";
 import adminRouter from "./routers/adminRouter.js";
@@ -10,6 +9,7 @@ const app = express();
 app.use(bodyParser.json({ limit: "50mb" }));
 const PORT = process.env.PORT || 5000;
 app.use(express.json()); //json body parser
+app.use(cors({ origin: "*" }));
 
 app.listen(PORT, () => console.log(`server started at port: ${PORT}`));
 
